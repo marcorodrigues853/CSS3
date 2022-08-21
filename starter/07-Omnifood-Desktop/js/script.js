16242c58db8d7ef8c81a42a6a4454e21e81de651
Marco Rodrigues <marcoaurelio853@gmail.com>
2022-08-21 11:46:37 +0100

    fixed menu on desktop

--- starter/07-Omnifood-Desktop/js/script.js
+++ starter/07-Omnifood-Desktop/js/script.js
@@ -1,3 +1,9 @@
+// console.log("Hello world!");
+
+// const myName = "Jonas Schmedtmann";
+// const h1 = document.querySelector(".heading-primary");
+// console.log(myName);
+// console.log(h1);
 
 // h1.addEventListener("click", function () {
 //   h1.textContent = myName;
